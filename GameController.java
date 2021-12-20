008130b6a8ee1d57a7afc8cc8682efe5e44a97bc
ionaaaa <95007793+ionaaaa@users.noreply.github.com>
2021-12-20 17:36:20 +0800

    Delete GameController.java

--- GameController.java
+++
@@ -1,9 +0,0 @@
-public class GameController {
-    public boolean isMousePressed() throws InterruptedException {
-        if (!StdDraw.isMousePressed()) {
-            return false;
-        } else
-            Thread.sleep(90);
-        return StdDraw.isMousePressed();
-    }
-}
